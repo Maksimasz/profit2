b01f1340005ac048a2da4dae66dd92b45ce5b7fc
Максим Азизов <maksimasz@yahoo.com>
2017-01-18 12:20:46 +0200

    добавили класс View

---
+++ App/View.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 18.01.2017
+ * Time: 12:19
+ */
+
+namespace App;
+
+
+class View
+{
+    protected $data = [];
+
+    public function __set($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->data[$name];
+    }
+
+    public function __isset($name)
+    {
+        return isset($this->data[$name]);
+    }
+
+    public function render($template)
+    {
+        foreach ($this->data as $key => $value) {
+            $$key = $value;
+        }
+        ob_start();
+        include $template;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+
+}
