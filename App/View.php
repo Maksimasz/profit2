4b1ba662044d3b3af990a82d8ba97bf0086e77e3
Максим Азизов <maksimasz@yahoo.com>
2017-01-24 15:37:56 +0200

    Начало

--- App/View.php
+++ App/View.php
@@ -4,7 +4,7 @@
 namespace App;
 
 
-class View
+class View implements \Countable, \Iterator
 {
     use Magic;
 
@@ -24,6 +24,60 @@ class View
         $content = ob_get_contents();
         ob_end_clean();
         return $content;
+    }
+
+    /**
+     * Count elements of an object
+     * @return int The custom count as an integer.
+     */
+    public function count()
+    {
+        return count($this->data);
+    }
+
+    /**
+     * Return the current element
+    @return mixed Can return any type.
+     */
+    public function current()
+    {
+        return current($this->data);
+    }
+
+    /**
+     * Move forward to next element
+     * @return void Any returned value is ignored.
+     */
+    public function next()
+    {
+        next($this->data);
+    }
+
+    /**
+     * Return the key of the current element
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key()
+    {
+        return key($this->data);
+    }
+
+    /**
+     * Checks if current position is valid
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     */
+    public function valid()
+    {
+        return null !== key($this->data);
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @return void Any returned value is ignored.
+     */
+    public function rewind()
+    {
+        return reset($this->data);
     }
 
 }
